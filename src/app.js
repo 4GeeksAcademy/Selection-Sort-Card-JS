dcd1fb918bb0a91c2afe6bfb0cfa59bde6270a9c
blancojohn <142819007+blancojohn@users.noreply.github.com>
2024-03-27 15:50:04 +0000

    Ordenamiento select sort listo en el navegador

--- src/app.js
+++ src/app.js
@@ -105,21 +105,70 @@ let botonSort = document.querySelector("#sort");
 console.log("BOTÓN", botonSort);
 
 function selectionSort() {
-  const len = cartasDesordenadas.length; /* len es la longitud de cartas a recorrer en el primer ciclo for */
+  const len =
+    cartasDesordenadas.length; /* len es la longitud de cartas a recorrer en el primer ciclo for */
   console.log("Cantidad de cartas a ordenar:", len);
+
+  let cartasOrdenadas = document.querySelector("#cartas-ordenadas");
+  console.log("DIV", cartasOrdenadas);
   for (let i = 0; i < len - 1; i++) {
-    let min = i; /* en este momento es todas las posiciones de mi array de cartas repartido. Por tanto inicio mi ciclo desde el primer elemento*/
-    console.log("Antes del for 2 como min valgo", min);
-    for (let j = i + 1; j < len; j++) { /* i + 1 por que mi primer elemento en este momento ya fué seleccionado en el primer for independientemente de su valor, simplemnte prevalece la posición. Por lo tanto,se va a comparar con el resto */
-      if (cartasDesordenadas[j].number < cartasDesordenadas[min].number) {/*  Además de reccorrer el tamaño accedo a los valores de las cartas por posición en el array y comparar si en el recorrido en una posición segín valor es menor que el último valor almacenado en la min. En el select comparo hasta el último elemento con lo que se va almacenando en la min*/
-        min = j;/* min va a valer lo que esté en la posición j de menor a mayor */
+    /* Inicia for desde el primer elemento porque necesito un punto de partida */
+    let min = i; /* almacena el valor de la posición 0 para comenzar comparación en el segundo for*/
+    for (let j = i + 1; j < len; j++) {
+      /* J inicia el for cada vez que la posición sea menor a las posiciones en len e inicia el ciclo en la siguiente posición por cada vuelta del for y evita comparar consigo mismo para compararse con el resto del array hasta la última posición */ /* este for encuentra el elemento menor */
+      if (cartasDesordenadas[j].number < cartasDesordenadas[min].number) {
+        /* Condiciona y accede a los valores del objeto carta por el atributo number para comparar en que posición está el atributo con valor menor */
+        min = j; /* min va a valer lo que esté en la posición j de menor a mayor */
       }
     }
-    console.log("luego del for 2 como min valgo", min);
     if (min !== i) {
+      /* condiciona para ejecutar el intercambio con los valores por atributo por posción actual de i. Porque en el primer for i es la longitud del array con el atributo number desordenado
+    En la primera condición se compara las posiciones por valor de atributo para ordenar de menor a mayor y se almacenan
+    en min */
       const temp = cartasDesordenadas[i];
       cartasDesordenadas[i] = cartasDesordenadas[min];
       cartasDesordenadas[min] = temp;
+    }
+    let contenedor = document.createElement("div");
+    contenedor.classList.add("d-flex");
+
+    for (let x = 0; x < len; x++) {
+      let cartaEnOrden = document.createElement("div");
+      cartaEnOrden.classList.add("card");
+
+      let cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+
+      let paloSuperior = document.createElement("div");
+      paloSuperior.classList.add("position-absolute", "top-0", "start-0");
+      paloSuperior.innerHTML = cartasDesordenadas[x].palo;
+
+      let numberCentral = document.createElement("div");
+      numberCentral.innerHTML = changeValiu(cartasDesordenadas[x].number);
+
+      let paloInferior = document.createElement("div");
+      paloInferior.classList.add("position-absolute", "bottom-0", "end-0");
+      paloInferior.innerHTML = cartasDesordenadas[x].palo;
+
+      cartasOrdenadas.appendChild(contenedor);
+      contenedor.appendChild(cartaEnOrden);
+      cartaEnOrden.appendChild(cardBody);
+      cardBody.appendChild(paloSuperior);
+      cardBody.appendChild(numberCentral);
+      cardBody.appendChild(paloInferior);
+
+      if (
+        cartasDesordenadas[x].palo == "♦" ||
+        cartasDesordenadas[x].palo == "♥"
+      ) {
+        paloSuperior.style.color = "red";
+        paloInferior.style.color = "red";
+        numberCentral.style.color = "red";
+      } else {
+        paloSuperior.style.color = "black";
+        paloInferior.style.color = "black";
+        numberCentral.style.color = "black";
+      }
     }
   }
   console.log("cartas ordenadas:", cartasDesordenadas);
